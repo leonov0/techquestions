0dd2190e24b601f1c15da144782e64998533ca12
Artyom Leonov <61983418+leonov0@users.noreply.github.com>
2024-10-28 14:26:07 +0200

    feat: add base auth and database configurations (#2)
    * chore: add next-auth dependency
    
    * chore: add drizzle dependencies
    
    * chore: add dotenv example
    
    * chore: add db scripts to package.json
    
    * chore: add drizzle config file
    
    * feat: setup database
    
    * feat: add users table
    
    * feat: setup auth.js
    
    * feat: update auth providers configuration

---
+++ src/features/auth.ts
@@ -0,0 +1,17 @@
+import { DrizzleAdapter } from "@auth/drizzle-adapter";
+import NextAuth from "next-auth";
+import GitHub from "next-auth/providers/github";
+import Google from "next-auth/providers/google";
+
+import { database } from "@/features/database";
+
+export const { handlers, signIn, signOut, auth } = NextAuth({
+  adapter: DrizzleAdapter(database),
+  providers: [
+    GitHub({ allowDangerousEmailAccountLinking: true }),
+    Google({ allowDangerousEmailAccountLinking: true }),
+  ],
+  session: {
+    strategy: "jwt",
+  },
+});
