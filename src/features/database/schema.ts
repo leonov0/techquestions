235906cdbfeea2911d9fbf77684e81aeabb5f0db
Artyom Leonov <6leonov@gmail.com>
2024-11-17 20:59:59 +0200

    feat: add company, level, technologies, and relationship tables

--- src/features/database/schema.ts
+++ src/features/database/schema.ts
@@ -1,3 +1,4 @@
+import { relations } from "drizzle-orm";
 import {
   boolean,
   integer,
@@ -66,6 +67,12 @@ export const questions = pgTable("question", {
 export type Question = typeof questions.$inferSelect;
 export type NewQuestion = typeof questions.$inferInsert;
 
+export const questionsRelations = relations(users, ({ many }) => ({
+  questionsToCompanies: many(questionsToCompanies),
+  questionsToTechnologies: many(questionsToTechnologies),
+  questionsToLevels: many(questionsToLevels),
+}));
+
 export const questionReviews = pgTable("question_review", {
   id: uuid("id").primaryKey().defaultRandom(),
   questionId: uuid("questionId")
@@ -83,3 +90,90 @@ export const questionReviews = pgTable("question_review", {
 
 export type QuestionReview = typeof questionReviews.$inferSelect;
 export type NewQuestionReview = typeof questionReviews.$inferInsert;
+
+export const companies = pgTable("company", {
+  id: uuid("id").primaryKey().defaultRandom(),
+  name: varchar("name", { length: 255 }),
+  createdAt: timestamp("createdAt", { mode: "date" }).notNull().defaultNow(),
+  updatedAt: timestamp("updatedAt", { mode: "date" }).notNull().defaultNow(),
+});
+
+export type Company = typeof companies.$inferSelect;
+export type NewCompany = typeof companies.$inferInsert;
+
+export const companiesRelations = relations(companies, ({ many }) => ({
+  questionsToCompanies: many(questionsToCompanies),
+}));
+
+export const questionsToCompanies = pgTable(
+  "questions_to_companies",
+  {
+    questionId: uuid("questionId")
+      .notNull()
+      .references(() => questions.id),
+    companyId: uuid("companyId")
+      .notNull()
+      .references(() => companies.id),
+  },
+  (t) => ({
+    pk: primaryKey({ columns: [t.questionId, t.companyId] }),
+  }),
+);
+
+export const technologies = pgTable("technology", {
+  id: uuid("id").primaryKey().defaultRandom(),
+  name: varchar("name", { length: 255 }),
+  createdAt: timestamp("createdAt", { mode: "date" }).notNull().defaultNow(),
+  updatedAt: timestamp("updatedAt", { mode: "date" }).notNull().defaultNow(),
+});
+
+export type Technology = typeof technologies.$inferSelect;
+export type NewTechnology = typeof technologies.$inferInsert;
+
+export const technologiesRelations = relations(technologies, ({ many }) => ({
+  questionsToTechnologies: many(questionsToTechnologies),
+}));
+
+export const questionsToTechnologies = pgTable(
+  "questions_to_technologies",
+  {
+    questionId: uuid("questionId")
+      .notNull()
+      .references(() => questions.id),
+    technologyId: uuid("technologyId")
+      .notNull()
+      .references(() => technologies.id),
+  },
+  (t) => ({
+    pk: primaryKey({ columns: [t.questionId, t.technologyId] }),
+  }),
+);
+
+export const levels = pgTable("level", {
+  id: uuid("id").primaryKey().defaultRandom(),
+  name: varchar("name", { length: 255 }),
+  createdAt: timestamp("createdAt", { mode: "date" }).notNull().defaultNow(),
+  updatedAt: timestamp("updatedAt", { mode: "date" }).notNull().defaultNow(),
+});
+
+export type Level = typeof levels.$inferSelect;
+export type NewLevel = typeof levels.$inferInsert;
+
+export const levelsRelations = relations(levels, ({ many }) => ({
+  questionsToLevels: many(questionsToLevels),
+}));
+
+export const questionsToLevels = pgTable(
+  "questions_to_levels",
+  {
+    questionId: uuid("questionId")
+      .notNull()
+      .references(() => questions.id),
+    levelId: uuid("levelId")
+      .notNull()
+      .references(() => levels.id),
+  },
+  (t) => ({
+    pk: primaryKey({ columns: [t.questionId, t.levelId] }),
+  }),
+);
