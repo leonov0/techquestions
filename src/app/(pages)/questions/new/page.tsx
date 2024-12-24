23074d4abcf72414cfc693df01f409b689f7d06e
Artyom Leonov <6leonov@gmail.com>
2024-12-24 15:58:11 +0200

    feat: add empty add question form component in new question page

--- src/app/(pages)/questions/new/page.tsx
+++ src/app/(pages)/questions/new/page.tsx
@@ -1,12 +1,15 @@
 import { Footer } from "@/components/layout/footer";
 import { Header } from "@/components/layout/header";
+import { AddQuestionForm } from "@/features/questions/add-question-form";
 
 export default function AddQuestion() {
   return (
     <div className="grid min-h-dvh grid-rows-[auto,_1fr,_auto]">
       <Header />
 
-      <main className="container py-16">Add a question</main>
+      <main className="container py-16">
+        <AddQuestionForm />
+      </main>
 
       <Footer />
     </div>
---
+++ src/features/questions/add-question-form.tsx
@@ -0,0 +1,3 @@
+export function AddQuestionForm() {
+  return <div>AddQuestionForm</div>;
+}
