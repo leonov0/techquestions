7927e9b8c0c4af937865a68a8fc4f5603f1febda
Artyom Leonov <6leonov@gmail.com>
2024-12-29 17:44:14 +0200

    style: format class names

--- src/app/(pages)/(home)/hero-section.tsx
+++ src/app/(pages)/(home)/hero-section.tsx
@@ -6,17 +6,17 @@ import { cn } from "@/lib/utils";
 export function HeroSection({ isAuthenticated }: { isAuthenticated: boolean }) {
   return (
     <section className="mx-auto mt-32 max-w-xl">
-      <h1 className="motion-translate-y-in-50 motion-opacity-in-[0%] text-3xl font-semibold tracking-tight">
+      <h1 className="text-3xl font-semibold tracking-tight motion-translate-y-in-50 motion-opacity-in-[0%]">
         Master Every Tech Interview
       </h1>
 
-      <p className="motion-translate-y-in-50 motion-opacity-in-[0%] motion-delay-100 mt-2 text-lg text-muted-foreground sm:text-xl">
+      <p className="mt-2 text-lg text-muted-foreground motion-translate-y-in-50 motion-opacity-in-[0%] motion-delay-100 sm:text-xl">
         A community-driven repository of real interview questions from top
         companies. Sort by company, experience level, and technology to prepare
         smarter, not harder.
       </p>
 
-      <div className="motion-translate-y-in-50 motion-opacity-in-[0%] motion-delay-300 mt-4 flex gap-4">
+      <div className="mt-4 flex gap-4 motion-translate-y-in-50 motion-opacity-in-[0%] motion-delay-300">
         <Link
           href="/questions"
           className={cn(buttonVariants(), "sm:h-10 sm:px-8")}
