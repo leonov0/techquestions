76f5900a7eca7493a67103f452a7dbceb6f66919
Artyom Leonov <6leonov@gmail.com>
2025-01-19 13:10:02 +0200

    feat: question section for question detail page and base loading

--- src/app/(pages)/questions/[id]/page.tsx
+++ src/app/(pages)/questions/[id]/page.tsx
@@ -1,9 +1,10 @@
-import { CrossCircledIcon } from "@radix-ui/react-icons";
+import { Suspense } from "react";
 
 import { Footer } from "@/components/layout/footer";
 import { Header } from "@/components/layout/header";
-import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert";
-import { getQuestion } from "@/features/get-question";
+
+import Loading from "./loading";
+import { QuestionSection } from "./question-section";
 
 export default async function Question({
   params,
@@ -12,32 +13,14 @@ export default async function Question({
 }) {
   const { id } = await params;
 
-  const { data, error } = await getQuestion(id);
-
   return (
     <div className="grid min-h-dvh grid-rows-[auto,_1fr,_auto]">
       <Header />
 
       <main className="container py-16">
-        {error && (
-          <Alert variant="destructive">
-            <CrossCircledIcon />
-
-            <AlertTitle>
-              An error occurred while fetching the questions.
-            </AlertTitle>
-
-            <AlertDescription>{error}</AlertDescription>
-          </Alert>
-        )}
-
-        {data && (
-          <div>
-            <h1 className="text-3xl font-bold">{data.title}</h1>
-
-            <p className="mt-4 text-lg">{data.body}</p>
-          </div>
-        )}
+        <Suspense fallback={<Loading />}>
+          <QuestionSection id={id} />
+        </Suspense>
       </main>
 
       <Footer />
