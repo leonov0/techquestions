890ec0f76c5644f84074c0c081bf0634a9e5a116
Artyom Leonov <6leonov@gmail.com>
2025-02-16 12:10:21 +0200

    feat: add SQL migration and update schema to set title, isAnonymous, and status columns as NOT NULL in question table

--- src/database/schema/questions.ts
+++ src/database/schema/questions.ts
@@ -20,14 +20,16 @@ export const questions = pgTable(
   "question",
   {
     id: uuid("id").primaryKey().defaultRandom(),
-    title: varchar("title", { length: 255 }),
+    title: varchar("title", { length: 255 }).notNull(),
     body: text("body"),
     userId: uuid("userId").references(() => users.id),
-    isAnonymous: boolean("isAnonymous").default(false),
+    isAnonymous: boolean("isAnonymous").notNull().default(false),
     status: varchar("status", {
       length: 32,
       enum: ["pending", "approved", "rejected"],
-    }).default("pending"),
+    })
+      .notNull()
+      .default("pending"),
     createdAt: timestamp("createdAt", { mode: "date" }).notNull().defaultNow(),
     updatedAt: timestamp("updatedAt", { mode: "date" }).notNull().defaultNow(),
   },
