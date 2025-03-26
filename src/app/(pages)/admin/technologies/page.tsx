16b106bb9c8d7cf1b64e6a598586eb1d90b878f6
Artyom Leonov <6leonov@gmail.com>
2025-03-26 18:49:57 +0200

    feat: add heading to manage technologies page

--- src/app/(pages)/admin/technologies/page.tsx
+++ src/app/(pages)/admin/technologies/page.tsx
@@ -4,8 +4,16 @@ import { ListLoader, TechnologyList } from "@/features/categories";
 
 export default function ManageTechnologies() {
   return (
-    <Suspense fallback={<ListLoader />}>
-      <TechnologyList />
-    </Suspense>
+    <div>
+      <h1 className="text-3xl font-semibold tracking-tight">
+        Manage technologies
+      </h1>
+
+      <section className="mt-8">
+        <Suspense fallback={<ListLoader />}>
+          <TechnologyList />
+        </Suspense>
+      </section>
+    </div>
   );
 }
