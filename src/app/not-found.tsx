c506ae243b8ef2564f3b2d8c41c52cc80d260b79
Artyom Leonov <6leonov@gmail.com>
2025-04-16 09:47:34 +0300

    feat: add not found page

---
+++ src/app/not-found.tsx
@@ -0,0 +1,22 @@
+import { Undo2 } from "lucide-react";
+import Link from "next/link";
+
+import { buttonVariants } from "@/components/ui/button";
+import { Separator } from "@/components/ui/separator";
+
+export default function NotFound() {
+  return (
+    <main className="flex h-dvh flex-col items-center justify-center gap-8">
+      <div className="flex items-center gap-4">
+        <span className="text-2xl leading-12">404</span>
+        <Separator orientation="vertical" />
+        <h1>This page could not be found.</h1>
+      </div>
+
+      <Link href="/" className={buttonVariants({ size: "lg" })}>
+        <Undo2 />
+        Back to Home
+      </Link>
+    </main>
+  );
+}
