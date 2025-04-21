f3c93bc154dfad2affa14c31af5b9f0289b2c01e
Artyom Leonov <6leonov@gmail.com>
2025-04-21 10:30:13 +0300

    feat: enhance send email function with development functionality

--- src/lib/resend.ts
+++ src/lib/resend.ts
@@ -1,3 +1,26 @@
-import { Resend } from "resend";
+import {
+  type CreateEmailOptions,
+  type CreateEmailRequestOptions,
+  type CreateEmailResponse,
+  Resend,
+} from "resend";
 
 export const resend = new Resend(process.env.RESEND_API_KEY);
+
+export function sendEmail(
+  payload: CreateEmailOptions,
+  options?: CreateEmailRequestOptions,
+): Promise<CreateEmailResponse> {
+  if (process.env.NODE_ENV === "development") {
+    console.info("Sending email: ", payload);
+
+    return Promise.resolve({
+      data: {
+        id: "mocked_email_id",
+      },
+      error: null,
+    });
+  }
+
+  return resend.emails.send(payload, options);
+}
