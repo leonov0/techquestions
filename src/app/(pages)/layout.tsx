682e760c9ea4b765936fdb8e61e3e13d53dd9ca5
Artyom Leonov <6leonov@gmail.com>
2025-04-21 10:31:13 +0300

    feat: rename layout function to pages layout and add gap to grid layout

--- src/app/(pages)/layout.tsx
+++ src/app/(pages)/layout.tsx
@@ -1,11 +1,15 @@
 import { Footer } from "@/components/layout/footer";
 import { Header } from "@/components/layout/header";
 
-export default function Layout({ children }: { children: React.ReactNode }) {
+export default function PagesLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   return (
-    <div className="grid min-h-dvh grid-rows-[auto_1fr_auto]">
+    <div className="grid min-h-dvh grid-rows-[auto_1fr_auto] gap-16">
       <Header />
-      {children}
+      <div>{children}</div>
       <Footer />
     </div>
   );
