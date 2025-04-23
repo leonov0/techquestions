8fac78307ccb2fc83b442e44d13a5401ec9720c9
Artyom Leonov <6leonov@gmail.com>
2025-04-23 15:20:26 +0300

    fix: hide author is question is anonymous

--- src/widgets/question-section.tsx
+++ src/widgets/question-section.tsx
@@ -6,7 +6,6 @@ import rehypeRaw from "rehype-raw";
 
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import { Separator } from "@/components/ui/separator";
-import { Skeleton } from "@/components/ui/skeleton";
 import { getQuestion } from "@/features/questions";
 import { Rating, RatingSkeleton } from "@/features/rating";
 import { getCapitalizedFirstLetter } from "@/lib/utils";
@@ -26,6 +25,10 @@ export async function QuestionSection({
 
   if (!response.success) {
     return notFound();
+  }
+
+  if (response.data.isAnonymous) {
+    response.data.author = null;
   }
 
   return (
@@ -100,33 +103,6 @@ export async function QuestionSection({
           {response.data.body}
         </ReactMarkdown>
       </div>
-    </section>
-  );
-}
-
-export function QuestionSectionLoader() {
-  return (
-    <section>
-      <Skeleton className="h-9 w-full" />
-
-      <div className="mt-2 flex flex-col gap-4 sm:flex-row">
-        <Skeleton className="h-6 w-56" />
-        <Skeleton className="h-6 w-56" />
-      </div>
-
-      {
-        <ul className="mt-4 flex flex-wrap gap-2">
-          {[...new Array(5)].map((_, index) => (
-            <Skeleton key={`badge-skeleton-${index}`} className="h-6 w-16" />
-          ))}
-        </ul>
-      }
-
-      <Skeleton className="mt-4 h-10 w-56" />
-
-      <Separator className="my-8" />
-
-      <Skeleton className="h-64 w-full" />
     </section>
   );
 }
