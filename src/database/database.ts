6bb39b60eb41bae7a66cdc2ee50f58237eaaa155
Artyom Leonov <6leonov@gmail.com>
2025-04-29 12:34:50 +0300

    feat: update database connection to use pool from neon-serverless

--- src/database/database.ts
+++ src/database/database.ts
@@ -1,8 +1,8 @@
-import { neon } from "@neondatabase/serverless";
-import { drizzle } from "drizzle-orm/neon-http";
+import { Pool } from "@neondatabase/serverless";
+import { drizzle } from "drizzle-orm/neon-serverless";
 
 import * as schema from "./schema";
 
-const sql = neon(process.env.DATABASE_URL!);
+const pool = new Pool({ connectionString: process.env.DATABASE_URL! });
 
-export const database = drizzle(sql, { schema });
+export const database = drizzle(pool, { schema });
